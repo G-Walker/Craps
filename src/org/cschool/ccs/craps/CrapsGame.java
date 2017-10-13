003bea069b57998d178ac246c1ffb649783d6948
goro <foo@goro.com>
2017-10-13 10:52:07 -0400

    Initial Commit

---
+++ src/org/cschool/ccs/craps/CrapsGame.java
@@ -0,0 +1,71 @@
+package org.cschool.ccs.craps;
+
+public class CrapsGame {
+
+	/**
+	 * 
+	 */
+	public CrapsGame() {
+		super();
+	}
+
+	/**
+	 * TODO 
+	 * 
+	 * 
+	 * 
+	 * @param numDice
+	 * @return
+	 */
+	private int rollDice(int numDice) {
+		
+		/* TODO */
+		
+		
+		
+		return -1;
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void play() {
+		
+		/* first roll */
+		
+		/* if 7 or 11, win */
+		
+		/* if 2, 3, or 12, lose */
+		
+		/* otherwise roll is the "point" to make */
+		
+	
+		
+		/* keep rolling until win or lose */
+		while( true /* TODO fix this */ ) {
+			
+			/* if 7 or 11, lose */
+			
+			
+			/* if matches pont, win */
+			
+			
+			/* otherwise, keep rolling */
+			
+			
+			
+		}
+		
+		
+		/* END */
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		new CrapsGame().play();
+	}
+}
